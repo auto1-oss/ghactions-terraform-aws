f7112a9d1fefe9d8a6b8084ee51feec27d2ae2b9
mainak90 <mainak.dhar@auto1.com>
2021-08-17 21:29:08 +0200

    Squashing all commits of today
    Revert useless commits
    
    Get caller identity
    
    Get caller identity
    
    Get caller identity with node modules
    
    I am seriously some idiot, banish me to the netherrealm
    
    Some tracing
    
    Moving the env var derision
    
    Moving the envvar function to all actions
    
    Moving the envvar function to all actions
    
    Using actions exec in init
    
    Using actions exec in init
    
    Using actions exec in apply
    
    Using actions exec in apply
    
    Adapting apply for use
    
    Adapting apply for use
    
    Adapting apply for use
    
    Adapting apply for use
    
    Adapting apply for use
    
    Adapting destroy for use

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ var process = require('process');
 (async () => {
     try {
         const action = core.getInput("command");
-        const aws_access_key_id = core.getInput("aws_access_key_id");
-        const aws_secret_access_key = core.getInput("aws_secret_access_key")
-        const region = core.getInput("aws_region")
         core.info(`Running command ${action}...`)
-        process.env['AWS_ACCESS_KEY_ID'] = `${aws_access_key_id}`;
-        process.env['AWS_SECRET_ACCESS_KEY'] = `${aws_secret_access_key}`;
-        process.env['AWS_DEFAULT_REGION'] = `${region}`;
         switch (action) {
             default:
                 core.info(`Undefined action: ${action}`);
